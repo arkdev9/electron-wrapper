25e126aa44c840a88d3b264ee1d3478926db8177
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-08-10 09:08:27 +0530

    v0.0.202

---
+++ build/precache-manifest.62a5f14b5b9b4aaf42a2a37ee54d4caa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b161184bd6f8b189b05e84c34095193",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6caa514aaa150b9b537",
+    "url": "./static/js/2.9362375e.chunk.js"
+  },
+  {
+    "revision": "5620cb34322b15d9c2c569b89cb18775",
+    "url": "./static/js/2.9362375e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4714eca5d891d0d4d20f",
+    "url": "./static/js/main.c2c1ed84.chunk.js"
+  },
+  {
+    "revision": "d069f8df14d6dec2b7e4",
+    "url": "./static/js/runtime-main.59b2140b.js"
+  }
+]);
