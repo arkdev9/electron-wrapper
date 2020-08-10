fb1f308c76aac68c224718783425688378b2838c
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-08-10 09:00:10 +0530

    isolation build

---
+++ build/precache-manifest.98269ef1c79d033d6ebc48f948d9e2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ffa59665a0f16bb9a963decef65346",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "./static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "4d54dede30c4f210a967",
+    "url": "./static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "./static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "4d54dede30c4f210a967",
+    "url": "./static/js/main.763190c2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
